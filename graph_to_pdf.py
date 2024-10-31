2d7ddeff703aee7c6f2815c2dcf10a125012c1b1
eternaltensa <133330759+eternaltensa@users.noreply.github.com>
2024-10-31 17:54:54 +0500

    Update graph_to_pdf.py

--- graph_to_pdf.py
+++ graph_to_pdf.py
@@ -3,24 +3,24 @@ from reportlab.pdfgen import canvas
 import schedule
 from apscheduler.schedulers.blocking import BlockingScheduler
 import telebot
-bot_token = '6581480693:AAFSrGw0E9QTtNJU1PbJ_Ii65-e2nnvKsJ8'
+bot_token = 'token'
 bot = telebot.TeleBot(bot_token)
 chat_id= '-1001900597631'
 
 def doc():
     pdf_file = "Monthly_report.pdf"
-    page_width = 4000  # Ширина страницы в пикселях
+    page_width = 4000  # ширина страницы в пикселях
     page_height = 3900
     pagesize=(page_width, page_height)
     c = canvas.Canvas(pdf_file, pagesize=pagesize)
 
-    # Определите функцию для добавления изображения с черным фоном
+    #добавление изображения с черным фоном
     def add_image_with_black_background(c, image_path, x, y, width, height):
-        c.setFillColorRGB(0, 0, 0)  # Устанавливаем черный фон
-        c.rect(x, y, width, height, fill=1)  # Рисуем черный прямоугольник
+        c.setFillColorRGB(0, 0, 0)  
+        c.rect(x, y, width, height, fill=1)  
         c.drawImage(image_path, x, y, width, height)
 
-    # Создайте список изображений и их координат
+    #список изображений и их координат
     images = [
         {"path": "monthly_template_heat.png", "x": 0, "y": 2400, "width": 2000, "height": 1500},
         {"path": "monthly_project_heat.png", "x": 2000, "y": 2400, "width": 2000, "height": 1500},
@@ -30,11 +30,11 @@ def doc():
         {"path": "monthly_project_pie.png", "x": 2000, "y": 0, "width": 2000, "height": 1200},
     ]
 
-    # Добавьте изображения с черным фоном в документ
+    #изображения с черным фоном в документ
     for img_info in images:
         add_image_with_black_background(c, img_info["path"], img_info["x"], img_info["y"], img_info["width"], img_info["height"])
 
-    # Завершите создание PDF-документа
+    #создание PDF-документа
     c.save()
 
     print(f"PDF-файл {pdf_file} создан успешно.")
@@ -44,7 +44,7 @@ def doc():
 
 doc()
 # scheduler = BlockingScheduler()
-# # # # Каждое первое число месяца в 00:40
+# # # # каждое первое число месяца в 00:40
 # scheduler.add_job(doc, 'cron', day='1', hour='0', minute='40')
 # scheduler.start()
 
