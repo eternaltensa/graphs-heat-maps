7558d5ced2817c0f009a1b6098641f70171cc6ab
eternaltensa <133330759+eternaltensa@users.noreply.github.com>
2025-03-12 16:49:57 +0500

    Update graph_to_pdf.py

--- graph_to_pdf.py
+++ graph_to_pdf.py
@@ -9,18 +9,17 @@ chat_id= ''
 
 def doc():
     pdf_file = "Monthly_report.pdf"
-    page_width = 4000  # ширина страницы в пикселях
+    page_width = 4000  
     page_height = 3900
     pagesize=(page_width, page_height)
     c = canvas.Canvas(pdf_file, pagesize=pagesize)
 
-    #добавление изображения с черным фоном
+
     def add_image_with_black_background(c, image_path, x, y, width, height):
         c.setFillColorRGB(0, 0, 0)  
         c.rect(x, y, width, height, fill=1)  
         c.drawImage(image_path, x, y, width, height)
 
-    #список изображений и их координат
     images = [
         {"path": "monthly_template_heat.png", "x": 0, "y": 2400, "width": 2000, "height": 1500},
         {"path": "monthly_project_heat.png", "x": 2000, "y": 2400, "width": 2000, "height": 1500},
@@ -30,11 +29,9 @@ def doc():
         {"path": "monthly_project_pie.png", "x": 2000, "y": 0, "width": 2000, "height": 1200},
     ]
 
-    #изображения с черным фоном в документ
     for img_info in images:
         add_image_with_black_background(c, img_info["path"], img_info["x"], img_info["y"], img_info["width"], img_info["height"])
 
-    #создание PDF-документа
     c.save()
 
     print(f"PDF-файл {pdf_file} создан успешно.")
@@ -43,8 +40,5 @@ def doc():
 
 
 doc()
-# scheduler = BlockingScheduler()
-# # # # каждое первое число месяца в 00:40
-# scheduler.add_job(doc, 'cron', day='1', hour='0', minute='40')
-# scheduler.start()
+
 
